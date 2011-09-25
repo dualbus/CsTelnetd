7c20844243e2acd8d6cf3dddf66bad04d0df5e81
Eduardo A. Bustamante López <dualbus@gmail.com>
2011-09-25 15:34:40 -0700

    Add comments to the source code (fix #1).
    This commit adds comments to the source code, as requested in issue #1 by Arturo Sevilla.

--- Telnet/Program.cs
+++ Telnet/Program.cs
@@ -9,18 +9,27 @@ using System.Threading;
 
 namespace Telnetd
 {
+    // This is the program's starting point (i.e. its "Main" class).
     public class Program
     {
+        // Port we will be listening to.
         private static readonly int _port = 9999;
+        // Shell to execute on each connection.
         private static readonly string _command = "cmd.exe";
 
         static void Main(string[] args)
         {
+            // Create a new TCP/IPv4 socket, bind it to 0.0.0.0:_port, so we can listen for requests on all
+            // network interfaces. Put the socket on listening mode, and accept connections for ever.
             Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             serverSocket.Bind((EndPoint)new IPEndPoint(IPAddress.Any, _port));
             serverSocket.Listen(5);
             while (true)
             {
+                // Each accepted connection will create a non-blocking thread, so the main thread can continue to listen on
+                // the server socket for new connections. The child thread receives the new socket created by accept, and
+                // the command to execute. We don't track these threads, so the server has no way to communicate with them or
+                // know how many clients we're handling at the time.
                 Socket clientConnection = serverSocket.Accept();
                 System.Console.WriteLine(string.Format("{0} {1}", clientConnection.RemoteEndPoint, DateTime.Now));
                 Thread thread = new Thread(new ThreadStart(() => 
@@ -32,11 +41,20 @@ namespace Telnetd
         }
     }
 
+    // This class handles an accepted connection.
+    //
+    // Its parameters are the socket created by accept, and a command to execute on the server machine.
     public class Connection
     {
         private Socket _socket;
         private ProcessStartInfo _processStartInfo;
 
+        // The constructor.
+        //
+        // This method stores the accepted socket as a private instance field, and initializes a new
+        // System.Diagnostics.ProcessStartInfo object that we can pass to System.Diagnostics.Process to start
+        // a process on the server machine. All of the standard file descriptors of the process are redirected,
+        // because we will connect them to the socket.
         public Connection(Socket socket, string command)
         {
             _socket = socket;
@@ -47,6 +65,12 @@ namespace Telnetd
             _processStartInfo.RedirectStandardError = true;
         }
 
+        // Method passed to ThreadStart.
+        //
+        // This is the thread's starting point. It starts the process on the server machine, creates three ``state objects'', which will
+        // help us handle the redirections asynchronously. We associate the BeginRead methods of the standard output and error streams
+        // with our call-back (LocalReadCallback), to start processing the output of the command. We also associate the BeginRead() method
+        // the socket to our socket read call-back (ReadCallback), so we can process the data received through the socket.
         public void DoWork()
         {
                 Process process = Process.Start(_processStartInfo);
@@ -60,6 +84,9 @@ namespace Telnetd
                 _socket.Close();
         }
 
+        // Asynchronous read call-back for the socket object.
+        //
+        // This call-back method fills the state object's buffer with the received data, and writes it to the process' standard input.
         private void ReadCallback(IAsyncResult iAsyncResult)
         {
             StateObject stateObject = (StateObject)iAsyncResult.AsyncState;
@@ -69,6 +96,7 @@ namespace Telnetd
                 int read = socket.EndReceive(iAsyncResult);
                 if (0 < read)
                 {
+                    // Handle the received data and reassociate the call-back with the socket's BeginReceive().
                     StreamWriter streamWriter = stateObject.Process.StandardInput;
                     streamWriter.Write(Encoding.UTF8.GetString(stateObject.Buffer, 0, read));
                     socket.BeginReceive(stateObject.Buffer, 0, StateObject.BUFFER_SIZE, 0, new AsyncCallback(ReadCallback), stateObject);
@@ -80,11 +108,16 @@ namespace Telnetd
             }
             catch(ObjectDisposedException)
             {
+                // If we reach this point, it means that either the socket or the process' standard input were closed, so we have nothing
+                // else to do here. Close the socket and the process, just in case.
                 stateObject.Socket.Close();
                 stateObject.Process.Close();
             }
         }
 
+        // Asynchronous read call-back for the standard output and error streams of the executed process.
+        //
+        // This call-back method fills the state object's buffer with the received data, and writes it to the socket.
         private void LocalReadCallback(IAsyncResult iAsyncResult)
         {
             LocalStateObject localStateObject = (LocalStateObject)iAsyncResult.AsyncState;
@@ -95,10 +128,13 @@ namespace Telnetd
                 int read = stream.EndRead(iAsyncResult);
                 if (0 < read)
                 {
+                    // Lock the socket, just in case the other call-back wants to write at the same time.
                     lock (socket)
                     {
                         socket.Send(localStateObject.Buffer, read, 0);
                     }
+
+                    // Reassociate the stream's BeginRead() method with the LocalReadCallback().
                     stream.BeginRead(localStateObject.Buffer, 0, LocalStateObject.BUFFER_SIZE, new AsyncCallback(LocalReadCallback), localStateObject);
                 }
                 else
@@ -108,13 +144,17 @@ namespace Telnetd
             }
             catch (ObjectDisposedException)
             {
+                // If we reach this point, it means that either the socket or the processed stream were closed, so we have nothing
+                // else to do here. Close the socket, the process, and the stream, just in case.
                 localStateObject.Socket.Close();
                 localStateObject.Process.Close();
                 localStateObject.Stream.Close();
             }
         }
 
-
+        // This is a helper class for the ReadCallback() method.
+        //
+        // We keep a reference to the socket, and the process, and a moderate sized buffer.
         private class StateObject
         {
             public const int BUFFER_SIZE = 1024;
@@ -153,6 +193,9 @@ namespace Telnetd
             }
         }
 
+        // This is a helper class for the LocalReadCallback() method.
+        //
+        // We keep a reference to the socket, the process, the handled stream, and a moderate sized buffer.
         private class LocalStateObject : StateObject
         {
             protected Stream _stream;
